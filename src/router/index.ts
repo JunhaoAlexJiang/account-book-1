44900041f4f3b2175d51ba7419d8245854ed6e61
Alexjiang <844670507@qq.com>
2022-03-14 16:30:15 +0800

    导航

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,32 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Money from "@/views/MoneyView.vue";
+import Label from "@/views/LabelView.vue";
+import Statistics from "@/views/StatisticsView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    redirect: "/money",
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/money",
+    component: Money,
+  },
+  {
+    path: "/label",
+    component: Label,
+  },
+  {
+    path: "/statistics",
+    component: Statistics,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/MoneyView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    记账
+    <hr />
+    <Nav />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "money",
+};
+</script>
+
+<style lang="scss" scoped></style>

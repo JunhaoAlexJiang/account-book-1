f80632339029891dbbe8eafeb5d8ef1fdea791c1
Alexjiang <844670507@qq.com>
2022-03-26 18:55:23 +0800

    计算器数字输入功能完成

--- src/components/MoneyView/NumberPad.vue
+++ src/components/MoneyView/NumberPad.vue
@@ -1,31 +1,57 @@
 <template>
   <div>
-    <div class="number">0</div>
+    <div class="number">{{ output }}</div>
     <div class="numberPad">
-      <el-button>1</el-button>
-      <el-button>2</el-button>
-      <el-button>3</el-button>
+      <el-button @click="inputContent">1</el-button>
+      <el-button @click="inputContent">2</el-button>
+      <el-button @click="inputContent">3</el-button>
       <el-button>删除</el-button>
 
-      <el-button>4</el-button>
-      <el-button>5</el-button>
-      <el-button>6</el-button>
+      <el-button @click="inputContent">4</el-button>
+      <el-button @click="inputContent">5</el-button>
+      <el-button @click="inputContent">6</el-button>
       <el-button>清空</el-button>
 
-      <el-button>7</el-button>
-      <el-button>8</el-button>
-      <el-button>9</el-button>
+      <el-button @click="inputContent">7</el-button>
+      <el-button @click="inputContent">8</el-button>
+      <el-button @click="inputContent">9</el-button>
       <el-button class="ok">ok</el-button>
 
-      <el-button>.</el-button>
-      <el-button>0</el-button>
-      <el-button>00</el-button>
+      <el-button @click="inputContent">.</el-button>
+      <el-button @click="inputContent">0</el-button>
+      <el-button @click="inputContent">00</el-button>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class NumberPad extends Vue {
+  output = "0";
+  inputContent(event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+    const input = button.textContent as string;
+    if (this.output.length >= 7) {
+      return;
+    }
+    if (this.output === "0") {
+      if ("0123456789".indexOf(input) >= 0) {
+        this.output = input;
+      } else if (".".indexOf(input) >= 0) {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf(".") >= 0 && input === ".") {
+      return;
+    }
+
+    this.output += input;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +73,7 @@ export default {};
     width: 25%;
     height: 8vh;
     float: left;
+    font-size: 2.5vh;
   }
   > .ok {
     height: 16vh;
